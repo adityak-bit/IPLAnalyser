9c688b0e926808705dac68087e6d37250c57795d
adityak-bit <adityajsr1998@gmail.com>
2020-10-30 00:43:18 +0530

    [ADITYA] Added : Test Case to get Bowler Having Top Bowling Average

--- test/java/com/cg/IPLBowlerTest.java
+++ test/java/com/cg/IPLBowlerTest.java
@@ -1,17 +1,33 @@
 package com.cg;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class IPLBowlerTest {
 
 	public static final String BOWLER_STATS_FILE_PATH = "C:/Users/91824/eclipse-workspace/IPLAnalyser/BowlerStats.csv";
 	
+	private IPLAnalyser iplAnalyser;
+	List<BowlerStatsCSV> bowlingList = new ArrayList<BowlerStatsCSV>();
+	List<BowlerStatsCSV> sortedList;
+
+	@Before
+	public void initialize() throws IPLException {
+		iplAnalyser = new IPLAnalyser();
+		bowlingList = iplAnalyser.loadCsvFile(BOWLER_STATS_FILE_PATH, BowlerStatsCSV.class);
+		sortedList = new ArrayList<BowlerStatsCSV>();
+	}
 	@Test
 	public void givenBowlerStatsCSV_ShouldReturnList() throws IPLException {
-		CSVFileLoader csvFileLoader = new CSVFileLoader();
-		List<BowlerStatsCSV> bowlerList = csvFileLoader.loadCSVFile(BOWLER_STATS_FILE_PATH,BowlerStatsCSV.class);
-		Assert.assertEquals(101, bowlerList.size());
+		Assert.assertEquals(101, bowlingList.size());
+	}
+	
+	@Test
+	public void givenBowlingData_shouldReturnTopBowlingAvg() {
+		sortedList = iplAnalyser.sortBowlingData(bowlingList, BowlingDataSorter.TOP_BOWLING_AVG);
+		Assert.assertEquals("Krishnappa Gowtham", sortedList.get(98).getPlayer());
 	}
 }
